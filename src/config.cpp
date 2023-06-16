cb78ead22b8ceb7ab361139e424c3ce1e957c4c9
Ammar Ratnani <ammrat13@gmail.com>
2023-06-16 18:59:05 -0400

    Use default values if no configuration exists

--- src/config.cpp
+++ src/config.cpp
@@ -21,26 +21,26 @@ namespace lc32sim {
         // Check for existence of config file
         std::ifstream config_file(CONFIG_FILE_NAME);
         if (!config_file.good()) {
-            std::cout << "No config file found" << std::endl;
-            return;
+            std::cout << "No config file found, using defaults" << std::endl;
+
+        } else {
+            boost::property_tree::ptree data;
+            boost::property_tree::read_json(config_file, data);
+
+            try {
+                get_config(display.width, int);
+                get_config(display.height, int);
+                get_config(display.hblank_length, int);
+                get_config(display.vblank_length, int);
+                get_config(display.frames_per_second, double);
+                get_config(display.accelerated_rendering, bool);
+                get_config(allow_unaligned_access, bool);
+            } catch (boost::property_tree::json_parser::json_parser_error &e) {
+                throw SimulatorException("JSON parsing error: " + std::string(e.what()));
+            }
+
+            std::cout << "Config loaded" << std::endl;
         }
-
-        boost::property_tree::ptree data;
-        boost::property_tree::read_json(config_file, data);
-
-        try {
-            get_config(display.width, int);
-            get_config(display.height, int);
-            get_config(display.hblank_length, int);
-            get_config(display.vblank_length, int);
-            get_config(display.frames_per_second, double);
-            get_config(display.accelerated_rendering, bool);
-            get_config(allow_unaligned_access, bool);
-        } catch (boost::property_tree::json_parser::json_parser_error &e) {
-            throw SimulatorException("JSON parsing error: " + std::string(e.what()));
-        }
-
-        std::cout << "Config loaded" << std::endl;
         std::cout << "\tDisplay width: " << display.width << std::endl;
         std::cout << "\tDisplay height: " << display.height << std::endl;
         std::cout << "\tDisplay hblank length: " << display.hblank_length << std::endl;
