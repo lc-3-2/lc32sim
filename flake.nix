34d168a8a6114cd6ce84e2cf309f1bfdee0978ae
Ammar Ratnani <ammrat13@gmail.com>
2023-06-16 16:02:13 -0400

    Add flake files
    These files support using Nix Flakes for development. They allow the code
    to be built, but running with the GUI is not supported.

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "LC-3.2 Simulator Development and Build Environment";
+
+  inputs = {
+    nixpkgs-23-05.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs-23-05, systems, flake-utils }@inputs :
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system:
+    let
+      name = "lc32sim-dev";
+      pkgs = nixpkgs-23-05.legacyPackages.${system};
+      inherit (pkgs) stdenv;
+
+      buildInputs = [];
+      nativeBuildInputs = [
+        pkgs.cmake
+      ];
+      propagatedBuildInputs = [
+        pkgs.SDL2
+      ];
+    in {
+
+      devShells = rec {
+        default = lc32sim;
+
+        lc32sim = pkgs.mkShell {
+          inherit name buildInputs nativeBuildInputs propagatedBuildInputs;
+
+          shellHook = ''
+            export PS1="(${name}) [\u@\h \W]\$ "
+          '';
+        };
+      };
+    });
+}
