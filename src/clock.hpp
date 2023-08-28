9c42cd5c8d4c05e74d17c171dc87d49cd17e059d
Ammar Ratnani <ammrat13@gmail.com>
2023-08-27 19:22:57 -0500

    Add high-resolution timer
    The timer is at `REG_HIGHRESTIME_ADDR`, and it's the number of
    milliseconds that have elapsed since the current second started.

--- src/clock.hpp
+++ src/clock.hpp
@@ -12,8 +12,21 @@ namespace lc32sim {
             read_handlers get_read_handlers() override {
                 return {
                     { REG_CURRTIME_ADDR, [](uint32_t addr) -> uint32_t {
-                        return time(NULL);
-                    }}, 
+                        const auto now =
+                            std::chrono::system_clock::now().time_since_epoch();
+                        const auto now_sec =
+                            std::chrono::duration_cast<std::chrono::seconds>(now);
+                        return now_sec.count();
+                    }},
+                    { REG_HIGHRESTIME_ADDR, [](uint32_t addr) -> uint32_t {
+                        const auto now =
+                            std::chrono::system_clock::now().time_since_epoch();
+                        const auto now_milli =
+                            std::chrono::duration_cast<std::chrono::milliseconds>(now);
+                        const auto now_milli_rem =
+                            now_milli % std::chrono::milliseconds(std::chrono::seconds(1));
+                        return now_milli_rem.count();
+                    }},
                 };
             };
     };
