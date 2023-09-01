bab12cf43388df5c980b6969845cae96c929795d
Ammar Ratnani <ammrat13@gmail.com>
2023-09-01 17:02:07 -0500

    Implement new time interface

--- src/clock.hpp
+++ src/clock.hpp
@@ -5,25 +5,38 @@
 
 namespace lc32sim {
     class Clock : public IODevice {
+        private:
+            uint32_t time_mil = 0;
+            uint32_t time_sec = 0;
+
         public:
             std::string get_name() override { return "Clock"; };
             read_handlers get_read_handlers() override {
                 return {
-                    { REG_CURRTIME_ADDR, [](uint32_t addr) -> uint32_t {
+                    { CLOCK_MIL_ADDR, [this](uint32_t val) -> uint32_t {
+                        return this->time_mil;
+                    }},
+                    { CLOCK_SEC_ADDR, [this](uint32_t val) -> uint32_t {
+                        return this->time_sec;
+                    }},
+                };
+            };
+            write_handlers get_write_handlers() override {
+                return {
+                    {CLOCK_STATUS_ADDR, [this](uint32_t oldval, uint32_t val) -> uint32_t {
+                        // Get the time since epoch as a duration
                         const auto now =
                             std::chrono::system_clock::now().time_since_epoch();
+                        // Count the number of seconds and set that
                         const auto now_sec =
                             std::chrono::duration_cast<std::chrono::seconds>(now);
-                        return now_sec.count();
-                    }},
-                    { REG_HIGHRESTIME_ADDR, [](uint32_t addr) -> uint32_t {
-                        const auto now =
-                            std::chrono::system_clock::now().time_since_epoch();
-                        const auto now_milli =
+                        this->time_sec = now_sec.count();
+                        // Count the number of milliseconds and set that
+                        const auto now_mil =
                             std::chrono::duration_cast<std::chrono::milliseconds>(now);
-                        const auto now_milli_rem =
-                            now_milli % std::chrono::milliseconds(std::chrono::seconds(1));
-                        return now_milli_rem.count();
+                        this->time_mil = (now_mil % std::chrono::milliseconds(1000)).count();
+                        // Return zero for read
+                        return 0;
                     }},
                 };
             };
